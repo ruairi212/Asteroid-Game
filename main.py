4a5a4cfa83d66b2898681b901d9bacf6a6e90dd4
ruairi <ruairitierney212@gmail.com>
2024-08-19 11:26:26 +0100

    fps limiter began

--- main.py
+++ main.py
@@ -2,18 +2,15 @@ import pygame
 from constants import * 
 def main():
     pygame.init()
+    clock = pygame.time.Clock()
+    dt = 0
     screen = pygame.display.set_mode((SCREEN_WIDTH,SCREEN_HEIGHT))
-    loading()
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
         screen.fill("black")
         pygame.display.flip()
-
-def loading():
-    print("Starting asteroids!")
-    print("Screen width: {}".format(SCREEN_WIDTH))
-    print("Screen height: {}".format(SCREEN_HEIGHT))
+        dt = clock.tick(60) / 1000
 if __name__ == "__main__":
     main()
