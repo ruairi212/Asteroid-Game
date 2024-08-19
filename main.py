21720e340c1b01ac23d7343a7e33bebf55ac3d82
ruairi <ruairitierney212@gmail.com>
2024-08-19 10:42:25 +0100

    Constants!

--- main.py
+++ main.py
@@ -1,7 +1,10 @@
 import pygame
+from constants import * 
 def main():
     loading()
 def loading():
     print("Starting asteroids!")
+    print("Screen width:{}".format(SCREEN_WIDTH))
+    print("Screen height:{}".format(SCREEN_HEIGHT))
 if __name__ == "__main__":
     main()
