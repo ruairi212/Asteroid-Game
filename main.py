0bd90cd6e4b8fb31d8a83f9ebc463dcfd6577836
ruairi <ruairitierney212@gmail.com>
2024-08-19 10:52:25 +0100

    erm

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ def main():
     loading()
 def loading():
     print("Starting asteroids!")
-    print("Screen width:{}".format(SCREEN_WIDTH))
-    print("Screen height:{}".format(SCREEN_HEIGHT))
+    print("Screen width: {}".format(SCREEN_WIDTH))
+    print("Screen height: {}".format(SCREEN_HEIGHT))
 if __name__ == "__main__":
     main()
