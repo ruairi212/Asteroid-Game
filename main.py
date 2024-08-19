a5d62d986e4b1a0af6ca285a10977a0b45fa6a69
ruairi <ruairitierney212@gmail.com>
2024-08-19 11:13:05 +0100

    Gui created!

--- main.py
+++ main.py
@@ -1,7 +1,16 @@
 import pygame
 from constants import * 
 def main():
+    pygame.init()
+    screen = pygame.display.set_mode((SCREEN_WIDTH,SCREEN_HEIGHT))
     loading()
+    while True:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                return
+        screen.fill("black")
+        pygame.display.flip()
+
 def loading():
     print("Starting asteroids!")
     print("Screen width: {}".format(SCREEN_WIDTH))
